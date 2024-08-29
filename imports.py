d6761cdffc0a438c7ee5fd66483c4d5c9bb04119
Morgan 🏴󠁧󠁢󠁥󠁮󠁧󠁿 <107074986+N4GR@users.noreply.github.com>
2024-08-29 05:13:11 +0100

    ADDITION - Logging

--- imports.py
+++ imports.py
@@ -5,3 +5,22 @@ from PyQt6 import QtWidgets, QtGui, QtCore
 import ctypes
 import sys
 import logging
+
+# Local imports
+from util import logger
+
+def SetupLogging(name: str) -> logging.Logger:
+    """A function to initialise the logging of each file.
+
+    Args:
+        name (str): Name of the file being worked on.
+
+    Returns:
+        logging.Logger: Logger to use for logging.
+    """
+    logging = logger.Logger(name)
+    log = logging.log
+
+    sys.excepthook = logging.CustomExcepthook
+
+    return log
